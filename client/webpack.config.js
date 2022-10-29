316c322719630369f508993b71b3af5f717eb7b5
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-28 21:47:34 -0700

    creating manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,6 +31,25 @@ module.exports = () => {
       swSrc: './src-sw.js',
       swDest: 'src-sw.js',
     }),
+    // create manifest file
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just Another Text Editor',
+      short_name: 'JATE',
+      description: 'Just another text editor',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
     ],
 
     module: {
