759e3285342eff3d25f2651c7367e5e12356b1b1
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-28 21:29:02 -0700

    implement get db method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,6 +27,18 @@ export const putDb = async (content)  => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  
+  
+  //console.log('get from database');
+
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
