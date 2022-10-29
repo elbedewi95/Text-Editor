745b77a564d63d43008a9783dfa08427ad99f4af
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-28 22:12:13 -0700

    edit and rebuild

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,7 +3,9 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-   
+    console.log('click')
+    console.log("event" + event)
+    event.preventDefault();
     // Store the triggered events
     window.deferredPrompt = event;
 
@@ -13,7 +15,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-
+    console.log("i clicked");
     const promptEvent = window.deferredPrompt;
 
     // console.log(promptEvent)
