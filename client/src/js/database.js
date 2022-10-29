10ca034e685be103ed0e437fa3e8dfcedda0eb0b
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-28 21:30:34 -0700

    add comments to putdb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,11 +17,15 @@ export const putDb = async (content)  => {
 
 
   //console.log('add to database');
-
+    // Create a connection to the database database and version we want to use.
   const contactDb = await openDB('jate', 1);
+   // Create a new transaction and specify the database and data privileges.
   const tx = contactDb.transaction('jate', 'readwrite');
+ // Open up the desired object store.
   const store = tx.objectStore('jate');
+  // Use the .add() method on the store and pass in the content.
   const request = store.put({ id: 1, value: content });
+  // Get confirmation of the request.
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
