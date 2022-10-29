0adc694de193cbcf5c571a005b3a520fc1be3efa
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-28 21:49:02 -0700

    css and babel loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -54,7 +54,23 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          //css loader
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // babel-loader
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
